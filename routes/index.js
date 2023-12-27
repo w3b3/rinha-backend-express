9efd38062664734d526c2c94574dce8144485281
Daniel S <dsantos@mileiq.com>
2023-12-27 15:34:02 -0400

    formatting only

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // res.render('index', { title: 'Express' });
   // res.sendStatus(200);
-  res.send({ title: 'Express'});
+  res.send({ title: "Express" });
 });
 
 module.exports = router;
